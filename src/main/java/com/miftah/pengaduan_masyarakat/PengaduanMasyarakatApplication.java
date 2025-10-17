748ec15a503e32adfc2467790690946170e124f3
Karungg <miftahfadilah71@gmail.com>
2025-10-17 20:04:08 +0700

    Initial commit

---
+++ src/main/java/com/miftah/pengaduan_masyarakat/PengaduanMasyarakatApplication.java
@@ -0,0 +1,13 @@
+package com.miftah.pengaduan_masyarakat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengaduanMasyarakatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PengaduanMasyarakatApplication.class, args);
+	}
+
+}
