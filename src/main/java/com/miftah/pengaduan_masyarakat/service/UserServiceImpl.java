ace3dd4cc42fb315f262be62b65d2ce6bd1b1d00
Karungg <miftahfadilah71@gmail.com>
2025-10-23 10:20:28 +0700

    feat: add jwt service

--- src/main/java/com/miftah/pengaduan_masyarakat/service/UserServiceImpl.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/service/UserServiceImpl.java
@@ -11,7 +11,6 @@ import com.miftah.pengaduan_masyarakat.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
