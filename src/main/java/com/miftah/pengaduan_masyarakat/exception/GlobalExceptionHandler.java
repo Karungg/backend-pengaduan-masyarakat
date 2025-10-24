e9405527ab6090ddf0a40297455750ae5a2afcc1
Karungg <miftahfadilah71@gmail.com>
2025-10-24 09:29:04 +0700

    chore: add messages properties

--- src/main/java/com/miftah/pengaduan_masyarakat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/exception/GlobalExceptionHandler.java
@@ -1,6 +1,10 @@
 package com.miftah.pengaduan_masyarakat.exception;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -21,30 +25,43 @@ import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<GenericResponse<String>> handleIllegalArgumentException(IllegalArgumentException ex) {
         log.warn("Illegal argument exception: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.badRequest(ex.getMessage());
+
+        String message = messageSource.getMessage("error.illegal.argument", new Object[] { ex.getMessage() },
+                ex.getMessage(), LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.badRequest(message);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<GenericResponse<String>> handleResourceNotFoundException(ResourceNotFoundException ex) {
         log.warn("Resource not found: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.notFound(ex.getMessage());
+
+        String message = messageSource.getMessage("error.resource.notfound", new Object[] { ex.getMessage() },
+                ex.getMessage(), LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.notFound(message);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<GenericResponse<String>> handleUsernameNotFoundException(UsernameNotFoundException ex) {
         log.warn("Authentication failed - user not found: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.notFound(ex.getMessage());
+
+        String message = messageSource.getMessage("error.auth.usernotfound", new Object[] { ex.getMessage() },
+                ex.getMessage(), LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.notFound(message);
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
@@ -52,9 +69,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<GenericResponse<Map<String, List<String>>>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, List<String>> errors = new HashMap<>();
+        Locale currentLocale = LocaleContextHolder.getLocale();
         ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
+
+            String errorMessage = messageSource.getMessage(error, currentLocale);
             errors.computeIfAbsent(fieldName, k -> new ArrayList<>()).add(errorMessage);
         });
         log.warn("Validation failed: {}", errors);
@@ -62,51 +81,62 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<GenericResponse<String>> handleGenericException(Exception ex) {
-        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.INTERNAL_SERVER_ERROR,
-                "Terjadi kesalahan internal pada server.");
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    // JWT Exception
+    // JWT Exception handler
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<GenericResponse<String>> handleExpiredJwtException(ExpiredJwtException ex) {
         log.warn("JWT token has expired: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED,
-                "Token JWT telah kedaluwarsa.");
+        String message = messageSource.getMessage("error.jwt.expired", null, "Token JWT telah kedaluwarsa.",
+                LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED, message);
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
-    @ExceptionHandler({ SignatureException.class, MalformedJwtException.class })
+    @ExceptionHandler({ SignatureException.class, MalformedJwtException.class }) // Tangani signature atau format salah
     public ResponseEntity<GenericResponse<String>> handleInvalidJwtSignatureOrFormatException(JwtException ex) {
         log.warn("Invalid JWT token: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED, "Token JWT tidak valid.");
+        String message = messageSource.getMessage("error.jwt.invalid", null, "Token JWT tidak valid.",
+                LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED, message);
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(JwtException.class)
     public ResponseEntity<GenericResponse<String>> handleJwtException(JwtException ex) {
         log.error("Error processing JWT token: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED,
-                "Terjadi masalah saat memproses token.");
+        String message = messageSource.getMessage("error.jwt.processing", null, "Terjadi masalah saat memproses token.",
+                LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED, message);
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
+
+    // Spring security exception
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<GenericResponse<String>> handleAuthenticationException(AuthenticationException ex) {
         log.warn("Authentication failed: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED,
-                "Autentikasi gagal: " + ex.getMessage());
+
+        String message = messageSource.getMessage("error.auth.failed", new Object[] { ex.getMessage() },
+                "Autentikasi gagal.", LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.UNAUTHORIZED, message);
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<GenericResponse<String>> handleAccessDeniedException(AccessDeniedException ex) {
         log.warn("Access denied: {}", ex.getMessage());
-        GenericResponse<String> response = GenericResponse.error(HttpStatus.FORBIDDEN, "Akses ditolak.");
+        String message = messageSource.getMessage("error.access.denied", null, "Akses ditolak.",
+                LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.FORBIDDEN, message);
         return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<GenericResponse<String>> handleGenericException(Exception ex) {
+        log.error("An unexpected error occurred: {}", ex.getMessage(), ex);
+        String message = messageSource.getMessage("error.server.internal", null,
+                "Terjadi kesalahan internal pada server.", LocaleContextHolder.getLocale());
+        GenericResponse<String> response = GenericResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, message);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
