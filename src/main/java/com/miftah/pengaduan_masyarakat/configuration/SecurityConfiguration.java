0f32e34f75ea57876c239d99adf45d36d5185377
Karungg <miftahfadilah71@gmail.com>
2025-10-26 12:57:43 +0700

    chore: modify security cors configuration

--- src/main/java/com/miftah/pengaduan_masyarakat/configuration/SecurityConfiguration.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/configuration/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.miftah.pengaduan_masyarakat.configuration;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +32,7 @@ public class SecurityConfiguration {
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http.csrf(csrf -> csrf
                                 .disable())
+                                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                                 .authorizeHttpRequests(requests -> requests
                                                 .requestMatchers("/api/v1/auth/*")
                                                 .permitAll()
@@ -40,5 +45,17 @@ public class SecurityConfiguration {
                                                 UsernamePasswordAuthenticationFilter.class);
 
                 return http.build();
+        }
+
+        @Bean
+        public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+                configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
+                configuration.setAllowedHeaders(List.of("*"));
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
         }
 }
