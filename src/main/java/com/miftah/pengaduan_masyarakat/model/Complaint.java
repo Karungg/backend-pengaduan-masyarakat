ee50eb45977193f888dd6f2323de27bda70cf891
Karungg <miftahfadilah71@gmail.com>
2025-11-02 19:33:06 +0700

    chore: modify complaint model

--- src/main/java/com/miftah/pengaduan_masyarakat/model/Complaint.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/model/Complaint.java
@@ -1,5 +1,59 @@
 package com.miftah.pengaduan_masyarakat.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "complaints")
+@EntityListeners(AuditingEntityListener.class)
 public class Complaint {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(length = 255, nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String description;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private Instant date;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String location;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id", nullable = false, unique = false)
+    private User user;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "agency_id", nullable = false, unique = false)
+    private Agency agency;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
 }
