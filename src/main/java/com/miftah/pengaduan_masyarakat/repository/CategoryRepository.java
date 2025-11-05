d83217dbfb4f2de196668db96a5158933db70d04
Karungg <miftahfadilah71@gmail.com>
2025-11-05 10:22:28 +0700

    chore: add forgot repository annotation

--- src/main/java/com/miftah/pengaduan_masyarakat/repository/CategoryRepository.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.miftah.pengaduan_masyarakat.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.miftah.pengaduan_masyarakat.model.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
     boolean existsByName(String name);
--- src/main/java/com/miftah/pengaduan_masyarakat/repository/ComplaintRepository.java
+++ src/main/java/com/miftah/pengaduan_masyarakat/repository/ComplaintRepository.java
@@ -3,9 +3,11 @@ package com.miftah.pengaduan_masyarakat.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.miftah.pengaduan_masyarakat.model.Complaint;
 
+@Repository
 public interface ComplaintRepository extends JpaRepository<Complaint, UUID> {
 
     boolean existsByCategoryId(UUID categoryId);
